acac6fe57b8a4b68c43524e37033788255812e1b
root <voodik.am@gmail.com>
2014-11-05 17:44:25 +0400

    dsim write error fix.

--- drivers/pwm/pwm-samsung.c
+++ drivers/pwm/pwm-samsung.c
@@ -211,7 +211,7 @@ static int s3c_pwm_config(struct pwm_chip *chip, struct pwm_device *pwm,
 
 	period = NS_IN_HZ / period_ns;
 
-	pwm_info(s3c, "duty_ns=%d, period_ns=%d (%lu)\n",
+	pwm_dbg(s3c, "duty_ns=%d, period_ns=%d (%lu)\n",
 			duty_ns, period_ns, period);
 
 	/* Check to see if we are changing the clock rate of the PWM */
