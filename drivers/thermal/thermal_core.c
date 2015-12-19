eb05ce5d0956f91706ce73cb8b76411f0adab8ea
Anand Moon <linux.amoon@gmail.com>
2015-12-19 19:53:38 +0000

    thermal: core: handle tip tempeature on mode enable
    Handle the trip temperature when themal device mode is THERMAL_DEVICE_ENABLED
    
    Change-Id: Ibaefc19e781a1dcbf3c295630070433725cbd95b

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -436,12 +436,22 @@ static void update_temperature(struct thermal_zone_device *tz)
 
 void thermal_zone_device_update(struct thermal_zone_device *tz)
 {
-	int count;
+	int count, result;
+	enum thermal_device_mode mode;
 
 	update_temperature(tz);
 
-	for (count = 0; count < tz->trips; count++)
-		handle_thermal_trip(tz, count);
+	if (!tz->ops->get_mode)
+		return;
+
+	result = tz->ops->get_mode(tz, &mode);
+	if (result)
+		return;
+
+	if (mode == THERMAL_DEVICE_ENABLED) {
+		for (count = 0; count < tz->trips; count++)
+			handle_thermal_trip(tz, count);
+	}
 
 	/*
 	 * Alright, we handled this trip successfully.
