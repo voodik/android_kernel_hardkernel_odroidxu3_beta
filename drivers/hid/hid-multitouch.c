cdec3f54ea57872f33749f26505a41d6a1691abb
Chris KIM <codewalker@hardkernel.com>
2018-09-03 18:08:20 +0900

    ODROID-XU3/4: Add multitouch v/pid.
     /* WAVESHARE HDMI capacitive Screen */
     #define USB_VENDOR_ID_BYQDTECH^I0x0483
     #define USB_DEVICE_ID_BYQDTECH_7^I0x5750
    
    Change-Id: Ied83914cfa16d00a5cccedd2a68804a4b6058672
    (cherry picked from commit a9a88e1f672fa09ecae872d5b62e215a2c8586fa)

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -1791,6 +1791,12 @@ static const struct hid_device_id mt_devices[] = {
 				USB_VENDOR_ID_NHD,
 				USB_DEVICE_ID_NHD_7_0_HDMI_N_RSXN_CTU) },
 
+/* ByQDTech DISPLAY capacitive Screen */
+	{ .driver_data = MT_CLS_DEFAULT,
+		HID_USB_DEVICE(
+				USB_VENDOR_ID_BYQDTECH,
+				USB_DEVICE_ID_BYQDTECH_7) },
+
 	/* Generic MT device */
 	{ HID_DEVICE(HID_BUS_ANY, HID_GROUP_MULTITOUCH, HID_ANY_ID, HID_ANY_ID) },
 
